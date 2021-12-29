b8300f30f49acddb0c7cf8d81340e45c28dd0cdc
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-12-29 22:25:33 +0100

    adding registartion feature

--- src/pages/registration/RegistrationPage.tsx
+++ src/pages/registration/RegistrationPage.tsx
@@ -1,23 +1,104 @@
 import React, {useRef} from 'react'
 import Style from './RegistrationPage.module.scss'
+import {apiEndPoints} from '../../config/apiEndPoints'
+import axios from 'axios'
 
 import UserLogo from './../../assets/images/Benutzer.svg'
 import PasswordLogo from './../../assets/images/Passwortschloss.svg'
+import MailLogo from './../../assets/images/Mail.svg'
 
-interface LoginPageProps {
+
+interface RegistrationPageProps {
     setAppState: (value: string | ((prevVar: string) => string)) => void;
 }
+interface RegistrationFormEnteredData {
+    enteredUsername: string,
+    enteredEmail: string,
+    enteredPassword: string
+}
 
-const RegistrationPage: React.FC<LoginPageProps> = ({setAppState}) => {
+const RegistrationPage: React.FC<RegistrationPageProps> = ({setAppState}) => {
 
     const usernameInputRef = useRef<HTMLInputElement>(null)
     const usernameInputContainerRef = useRef<HTMLDivElement>(null)
+    const emailInputRef = useRef<HTMLInputElement>(null)
+    const emailInputContainerRef = useRef<HTMLDivElement>(null)
     const passwordInputRef = useRef<HTMLInputElement>(null)
     const passwordInputContainerRef = useRef<HTMLDivElement>(null)
 
+    //handleRegistrationRequest
     const handleRegistrationRequest = () => {
-
+        try {
+            isInputEmpty()
+            const enteredData = getInputData()
+            doUserRegistration(enteredData)
+        } catch (error) {
+            throw error
+        }
     }
+    const isInputEmpty = () => {
+        if(usernameInputRef.current!.value.trim() === ""){
+            usernameInputContainerRef.current!.style.border = '2px solid red'
+            const error = new Error('Username field is empty')
+            throw error
+        }
+        if(usernameInputRef.current!.value.trim() !== ""){
+            usernameInputContainerRef.current!.style.border = '1px solid black'
+        }
+        if(emailInputRef.current!.value.trim() === ""){
+            emailInputContainerRef.current!.style.border = '2px solid red'
+            const error = new Error('Email field is empty')
+            throw error
+        }
+        if(emailInputRef.current!.value.trim() !== ""){
+            emailInputContainerRef.current!.style.border = '1px solid black'
+        }
+        if(passwordInputRef.current!.value.trim() === ""){
+            passwordInputContainerRef.current!.style.border = '2px solid red'
+            const error = new Error('Password field is empty')
+            throw error
+        }
+        if(passwordInputRef.current!.value.trim() !== ""){
+            passwordInputContainerRef.current!.style.border = '1px solid black'
+        }
+        return true
+    }
+    const getInputData = ():RegistrationFormEnteredData => {
+        const username = usernameInputRef.current!.value
+        const email = emailInputRef.current!.value
+        const password = passwordInputRef.current!.value
+
+        return {
+            enteredUsername: username,
+            enteredEmail: email,
+            enteredPassword: password
+        }
+    }
+    const doUserRegistration = (enteredData: RegistrationFormEnteredData) => {
+        registerUser(enteredData).then( response => {
+            if(response.status === 200){
+                if (response.data.result === 'success'){
+                    alert('User Registration Successful')
+                } else if (response.data.result === 'duplicate'){
+                    alert('User Name Not Available')
+                }
+            }
+        }).catch( error => {
+            throw error
+        })
+    }
+    const registerUser = async (enteredData: RegistrationFormEnteredData) => {
+        return await axios.post(
+            apiEndPoints.userRegistration,
+            {
+                "username":enteredData.enteredUsername,
+                "email":enteredData.enteredEmail,
+                "password":enteredData.enteredPassword
+            }
+        )
+    }
+
+    //handleBackToLoginRequest
     const handleBackToLoginRequest = () => {
         setAppState('LoginPage')
     }
@@ -33,13 +114,13 @@ const RegistrationPage: React.FC<LoginPageProps> = ({setAppState}) => {
                         <div>
                             <img src={UserLogo} alt='UserLogo'></img>
                         </div>
-                        <input type='text' id='LoginFormUserInput' placeholder='username' ref={usernameInputRef}></input>
+                        <input type='text' placeholder='username' ref={usernameInputRef}></input>
                     </div>
-                    <div className={Style.InputContainerInner} ref={usernameInputContainerRef}>
+                    <div className={Style.InputContainerInner} ref={emailInputContainerRef}>
                         <div>
-                            <img src={UserLogo} alt='UserLogo'></img>
+                            <img src={MailLogo} alt='UserLogo'></img>
                         </div>
-                        <input type='text' id='LoginFormUserInput' placeholder='email' ref={usernameInputRef}></input>
+                        <input type='text' placeholder='email' ref={emailInputRef}></input>
                     </div>
                     <div className={Style.InputContainerInner} ref={passwordInputContainerRef}>
                         <div>
